c0e1964d498f3ad6cf23acc1119fd17ad20661ca
davidecitterio <davidecitt@hotmail.it>
2015-05-14 13:04:59 +0200

    creazione pagine html e alcune php

--- js/custom/menu.js
+++ js/custom/menu.js
@@ -3,29 +3,27 @@
 $(document).ready(ready);
 
 function ready(){
-    console.log("Downloading menu from DB..");
+    console.log("I'm ready!");
     var id=1;
 
     $.ajax({
         method: "POST",
         //dataType: "json", //type of data
         crossDomain: true, //localhost purposes
-        url: "../php/getMenu.php", //Relative or absolute path to file.php file
+        url: "./php/getMenu.php", //Relative or absolute path to file.php file
         data: {menu:id},
 
         success: function(response) {
             console.log(JSON.parse(response));
-
-            var courses=JSON.parse(response);
+            var menu=JSON.parse(response);
             var el="";
-
             for(var i=0;i<menu.length;i++){
 
-                el+="<li><a href='"+menu[i].link+"'>"+menu[i].name+"</a></li>";
-
+                    console.log("<li><a href='"+menu[i].link+"'>"+menu[i].name+"</a></li>");
+                     el+="<li><a href='"+menu[i].link+"'>"+menu[i].name+"</a></li>";
             }
 
-            $("#menu").html(el);
+            $(".collapse navbar-collapse").html(el);
         },
         error: function(request,error)
         {
@@ -34,3 +32,7 @@ function ready(){
     });
 
 }
+
+
+
+
